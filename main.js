db25be9d13ec0bdf62f3ad18abf21a33e72204ab
Alexander Hjelm <nick.nertova@gmail.com>
2018-02-26 12:41:29 +0100

    JSLint

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
-var game = new Kiwi.Game(null, 'Hello World', null, { renderer: Kiwi.RENDERER_CANVAS });
-var myState = new Kiwi.State('myState');
+var game = new Kiwi.Game(null, "Hello World", null, { renderer: Kiwi.RENDERER_CANVAS });
+var myState = new Kiwi.State("myState");
 
-let playGrid = new PlayGrid(myState);
+var playGrid = new PlayGrid(myState);
 
-var connection = new WebSocket('ws://localhost:12345');
+var connection = new WebSocket("ws://localhost:12345");
 connection.onopen = function (event) {
-  window.alert("Connected to server!");
-  connection.send("Client connected!");
+    window.alert("Connected to server!");
+    connection.send("Client connected!");
 };
 
 connection.sendAction = function(actionStr) {
-    let msgDict = { "action":actionStr }
-    let msg = JSON.stringify(msgDict);
+    var msgDict = { "action":actionStr };
+    var msg = JSON.stringify(msgDict);
     this.send(msg);
 };
 
@@ -33,12 +33,12 @@ connection.onmessage = function(e){
     }
 
 
-}
+};
 
 myState.preload = function(){
     Kiwi.State.prototype.preload.call(this);
-    this.addImage('bg','bg.png');
-    this.addImage('fg','fg.png');
+    this.addImage("bg","bg.png");
+    this.addImage("fg","fg.png");
     this.leftKey = Kiwi.Input.Keycodes.LEFT;
     this.rightKey = Kiwi.Input.Keycodes.RIGHT;
     this.upKey = Kiwi.Input.Keycodes.UP;
@@ -51,29 +51,27 @@ myState.preload = function(){
     this.special5Key = Kiwi.Input.Keycodes.FIVE;
     this.testKey = Kiwi.Input.Keycodes.T;
 
-}
+};
 
 myState.create = function(){
     Kiwi.State.prototype.create.call(this);
-		//this.logo = new Kiwi.GameObjects.StaticImage(this, this.textures['logo'], 100, 100);
-		//this.secondlogo = new Kiwi.GameObjects.StaticImage(this, this.textures['logo'], 200, 300);			
-		//this.addChild(this.logo);
-		//this.addChild(this.secondlogo);
+                //this.logo = new Kiwi.GameObjects.StaticImage(this, this.textures["logo"], 100, 100);
+                //this.secondlogo = new Kiwi.GameObjects.StaticImage(this, this.textures["logo"], 200, 300);
+                //this.addChild(this.logo);
+                //this.addChild(this.secondlogo);
 
     //Attach callback to be executed when a key is pressed.
-    this.game.input.keyboard.onKeyDown.add( 
-        myState.onPress, 
-        this );
+    this.game.input.keyboard.onKeyDown.add(myState.onPress, this);
 
     this.blocksArray = [];
     for (var i=0;i<playGrid.sizeX;i++) {
         this.blocksArray[i] = [];
         for (var j=0;j<playGrid.sizeY;j++) {
-            this.blocksArray[i][j] = new Kiwi.GameObjects.StaticImage(this, this.textures['bg'], 10 + i*24, 10 + j*24);
-	    this.addChild(this.blocksArray[i][j]);
+            this.blocksArray[i][j] = new Kiwi.GameObjects.StaticImage(this, this.textures["bg"], 10 + i*24, 10 + j*24);
+            this.addChild(this.blocksArray[i][j]);
         }
-    }  
-}
+    }
+};
 
 myState.update = function(){
     Kiwi.State.prototype.update.call(this);
@@ -87,10 +85,10 @@ myState.update = function(){
     for (var i=0;i<playGrid.sizeX;i++) {
         for (var j=0;j<playGrid.sizeY;j++) {
             if (playGrid.grid[i][j] == 0) {
-                this.blocksArray[i][j].atlas = this.textures['bg'];
+                this.blocksArray[i][j].atlas = this.textures["bg"];
             }
             else if (playGrid.grid[i][j] != 0) {
-                this.blocksArray[i][j].atlas = this.textures['fg'];
+                this.blocksArray[i][j].atlas = this.textures["fg"];
             }
         }
     }
@@ -100,39 +98,39 @@ myState.onPress = function(keyCode) {
     switch (keyCode) {
         case this.leftKey:
             playGrid.updateActivePos(-1,0);
-            connection.sendAction('left');
+            connection.sendAction("left");
             break;
         case this.rightKey:
             playGrid.updateActivePos(1,0);
-            connection.sendAction('right');
+            connection.sendAction("right");
             break;
         case this.upKey:
-            connection.sendAction('rotate');
+            connection.sendAction("rotate");
             playGrid.rotateActiveShape();
             break;
         case this.downKey:
-            connection.sendAction('down');
+            connection.sendAction("down");
             break;
         case this.spaceKey:
-            connection.sendAction('harddrop');
+            connection.sendAction("harddrop");
             break;
         case this.special1Key:
-            connection.sendAction('special1');
+            connection.sendAction("special1");
             break;
         case this.special2Key:
-            connection.sendAction('special2');
+            connection.sendAction("special2");
             break;
         case this.special3Key:
-            connection.sendAction('special3');
+            connection.sendAction("special3");
             break;
         case this.special4Key:
-            connection.sendAction('special4');
+            connection.sendAction("special4");
             break;
         case this.special5Key:
-            connection.sendAction('special5');
+            connection.sendAction("special5");
             break;
         case this.testKey:
-            connection.sendAction('test');
+            connection.sendAction("test");
             break;
     }
 };
